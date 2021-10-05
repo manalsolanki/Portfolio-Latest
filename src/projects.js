2955347d71557b1c8d21af0fc1a112e9fedd0cdc
manalsolanki <97manal@gmail.com>
2021-10-05 17:16:47 -0400

    Projects decription added.

--- src/projects.js
+++ src/projects.js
@@ -1,37 +1,36 @@
 const projects = [
     {
         "name": "Royal Canin",
-        "shortDescription": "While working at Winobell-Inc, our client was Royal Canin. Updated webpages according to their developer dictonary.",
+        "shortDescription": "While working at Winobell-Inc, our client was Royal Canin. Created as well updated webpages according to their own design language.",
         "link": "https://fulldev-vet-royalcanin-ca.cs28.force.com/",
         "imgSrc": "/royalCanin.png",
-        "Description":"",
-        
+        "Description":"Worked together with client, designers and engineers to gather requirement and plan the design and structure of front-end interface.Created new web pages and re-designed existing ones with the help of their own design language. Implemented mobile first approach in all the design implementation. Worked on enchaning webpages using Visualforce Pages",    
         "roles":"UI/UX Designer",
-        "Technologies":"HTML, developer Dictonary, Visual Force Pages"
+        "Technologies":"HTML, Visualforce Pages, Design Language"
     },
     {
         "name": "Web Lancers",
         "shortDescription": "WebLancer  is created using PHP and MySQL and it is a secure web application connecting Freelancers with Employers/Project owners who are looking for web services.",
         "link": "https://weblancers.herokuapp.com/home.php",
         "imgSrc": "/webLancers.png",
-        "Description": "I have created a replica of the Amazon website.This includes different modules like registration,sign up,cart where customers can add product to their cart and would be notified with an email on placing the order and an inventory clerk module through which the retailers can upload and manage their products.",
+        "Description":"WebLancer is a secure web application connecting Freelance Professional Web Developers with Employers/Project Owners who are looking for web services.Implemented Features like Registration , Login/Logout , subscription to newsletter ,adding a project and many more.",
         "github":"https://github.com/Ninja-Coders-PHP/WebLancer-Final",
         "roles":"Developer, Designer, Team Leader",
         "Technologies":"PHP, MySQL"
     },
     {
     "name": "Amazon Replica",
-    "shortDescription": "I have created a replica of the Amazon website using nodeJs , express handlerbars and Mongodb.This includes different modules like registration,sign up,cart where customers can add product to their cart and would be notified with an email on placing the order and an inventory clerk module through which the retailers can upload and manage their products.",
+    "Description": "I have created a replica of the Amazon website using nodeJs , express handlerbars and Mongodb.This includes different modules like registration,sign up,cart where customers can add product to their cart and would be notified with an email on placing the order and an inventory clerk module through which the retailers can upload and manage their products.",
     "link": "https://amazon-website-assignment.herokuapp.com/",
     "imgSrc": "/Amazon.png",
-    "Description": "I have created a replica of the Amazon website.This includes different modules like registration,sign up,cart where customers can add product to their cart and would be notified with an email on placing the order and an inventory clerk module through which the retailers can upload and manage their products.",
+    "shortDescription": "I have created a replica of the Amazon website.This includes different modules like registration,sign up,cart where customers can add product to their cart and would be notified with an email on placing the order and an inventory clerk module through which the retailers can upload and manage their products.",
     "github":"https://github.com/manalsolanki/Amazon-Assignment",
     "roles":"Developer, Designer",
     "Technologies":"NodeJs , Express handlerbars and Mongodb"
 },
 {
     "name": "Beats On Travel",
-    "shortDescription": "I have created a replica of the Amazon website using nodeJs , express handlerbars and Mongodb.This includes different modules like registration,sign up,cart where customers can add product to their cart and would be notified with an email on placing the order and an inventory clerk module through which the retailers can upload and manage their products.",
+    "shortDescription":"The Project “ Beats on Travel“ is a application which calculates the travel time on car(that is distance between one city to another) and according to that travel time a song is played.",
     "link": "https://beats-on-travel.netlify.app/",
     "imgSrc": "/beatsOnTravel.png",
     "Description": "I have created a replica of the Amazon website.This includes different modules like registration,sign up,cart where customers can add product to their cart and would be notified with an email on placing the order and an inventory clerk module through which the retailers can upload and manage their products.",
@@ -41,20 +40,20 @@ const projects = [
 },
 {
     "name": "Addidas Landing Page",
-    "shortDescription": "The main aim of this project was to design the landing page using Adobe tools.The tools which are used are XD for wireframes and photoshop for editing the photos as well as creating icons.Also have prepared the html version of this single page website",
+    "shortDescription": "The main aim of this project was to design the landing page using Adobe tools.",
     "link": "https://manalsolanki.github.io/landingpage-adidas/",
     "imgSrc": "/final-design.png",
-    "Description": "",
+    "Description": "The main aim of this project was to design the landing page using Adobe tools.The tools which are used are XD for wireframes and photoshop for editing the photos as well as creating icons.Also have prepared the html version of this single page website. Intially created the wireframes and them accordingly created the websites from it.",
     "github":"https://github.com/manalsolanki/landingpage-adidas",
     "roles":"Designer, Developer",
     "Technologies":"Adobe XD, Photoshop"
 },
 {
     "name": "Event Microsites",
-    "shortDescription": "Designed and developed this project for a theme of event microsites.Have created different web pages which consist of all the events that occuring ,inidvidual detail of each event , speakers biography and booking a ticket page.The desings are responsive so they are mobile friendly.",
+    "shortDescription": "Designed and developed this project for a theme of event microsites. Created wireframes for all the devices as well as implented them.",
     "link": "https://event-microsites.netlify.app",
     "imgSrc": "/event.png",
-    "Description": "",
+    "Description": "Designed and developed this project for a theme of event microsites. Have created different web pages which consist of all the events that occuring ,inidvidual detail of each event , speakers biography and booking a ticket page.The desings are responsive so they are mobile friendly.",
     "github":"https://github.com/manalsolanki/EventMicrosites",
     "roles":"Designer, Developer",
     "wireframes":"",
@@ -62,11 +61,11 @@ const projects = [
 },
 {
     "name": "Travelocity Replica",
-    "shortDescription": "Designed the layout as well as the logo of this website using Adobe XD and Illustrator respectively.Also prepared low and high fedility wireframes.After desiging developed the website keeping in mind the best practices of SEO as well as used the HTML5 and CSS3 format for designing. ",
+    "shortDescription":"Designed the layout as well as go. Also prepared wireframes and website using the best pratices.",
     "link": "https://manalsolanki.github.io/composite_project_1/",
     "imgSrc": "/travelocity.png",
-    "Description": "",
-    "github":"",
+    "Description": "Designed the layout as well as the logo of this website using Adobe XD and Illustrator respectively.Also prepared low and high fedility wireframes.After desiging developed the website keeping in mind the best practices of SEO as well as used the HTML5 and CSS3 format for designing.",
+    "github":"https://github.com/manalsolanki/composite_project_1",
     "roles":"Designer, Developer",
     "Technologies":"Adobe XD, Illustrator, HTML, CSS"
 },
