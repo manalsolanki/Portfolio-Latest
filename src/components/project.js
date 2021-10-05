2f821859b6c12685a81fde5b721a8b981e26f29b
manalsolanki <97manal@gmail.com>
2021-10-05 17:03:22 -0400

    Projects added to the module.

--- src/components/project.js
+++ src/components/project.js
@@ -1,43 +1,52 @@
 import classes from "../styles/content.module.css";
+import projectClasses from "../styles/project.module.css";
 import { useState } from "react";
 import { Modal } from "react-bootstrap";
 const Project = (props) => {
+    const gotoGithub = () => {window.open(props.project.github)}
+    const goLive = () => {window.open(props.project.link)}
     const [show, setShow] = useState(false);
-
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-    return(
+    return (
         <div>
-
-        
-        <div onClick={handleShow}>
-            {/* <a href={props.project.link}> */}
-                  <div className={classes.imgDiv}>
+            <div onClick={handleShow}>
+                {/* <a href={props.project.link}> */}
+                <div className={classes.imgDiv}>
                     <div className={classes.projectTitle}>{props.project.name}</div>
                     <img src={props.project.imgSrc} alt="project" />
-                  </div>
-                  <p>{props.project.shortDescription}</p>
+                </div>
+                <p>{props.project.shortDescription}</p>
                 {/* </a>  */}
-        </div>
-        <Modal show={show}>
+            </div>
+            <Modal show={show} size="lg" centered>
                 <Modal.Header>
-                    <Modal.Title>Title</Modal.Title>
+                    <Modal.Title>{props.project.name}</Modal.Title>
                 </Modal.Header>
 
                 <Modal.Body>
-                    <div className="text-center">
-                        
-                        <p >Price : <b></b></p>
+                    <div className="d-flex">
+                        <div className={projectClasses.modalText}>
+                        <p><b>Technologies :</b> {props.project.Technologies}</p>
+                            <p><b>Roles :</b> {props.project.roles}</p>
+                            <p>{props.project.Description}</p>
+                            <div>
+                               {gotoGithub && <button className={projectClasses.modalButton} onClick={gotoGithub}>GitHub</button>} 
+                            
+                            <button className={projectClasses.modalButton} onClick={goLive}>Demo</button>
+                            </div>
+                        </div>
+                        <img className={projectClasses.modalImg} src={props.project.imgSrc} alt="project"/>
                     </div>
 
                 </Modal.Body>
 
                 <Modal.Footer>
-                    <button className="btn" onClick={handleClose}>close</button>
+                    <button className="btn btn-primary" onClick={handleClose}>Close</button>
                 </Modal.Footer>
             </Modal>
 
-      </div>
+        </div>
     )
 }
 
--- src/projects.js
+++ src/projects.js
@@ -1,11 +1,43 @@
-const projects = [{
+const projects = [
+    {
+        "name": "Royal Canin",
+        "shortDescription": "While working at Winobell-Inc, our client was Royal Canin. Updated webpages according to their developer dictonary.",
+        "link": "https://fulldev-vet-royalcanin-ca.cs28.force.com/",
+        "imgSrc": "/royalCanin.png",
+        "Description":"",
+        
+        "roles":"UI/UX Designer",
+        "Technologies":"HTML, developer Dictonary, Visual Force Pages"
+    },
+    {
+        "name": "Web Lancers",
+        "shortDescription": "WebLancer  is created using PHP and MySQL and it is a secure web application connecting Freelancers with Employers/Project owners who are looking for web services.",
+        "link": "https://weblancers.herokuapp.com/home.php",
+        "imgSrc": "/webLancers.png",
+        "Description": "I have created a replica of the Amazon website.This includes different modules like registration,sign up,cart where customers can add product to their cart and would be notified with an email on placing the order and an inventory clerk module through which the retailers can upload and manage their products.",
+        "github":"https://github.com/Ninja-Coders-PHP/WebLancer-Final",
+        "roles":"Developer, Designer, Team Leader",
+        "Technologies":"PHP, MySQL"
+    },
+    {
     "name": "Amazon Replica",
     "shortDescription": "I have created a replica of the Amazon website using nodeJs , express handlerbars and Mongodb.This includes different modules like registration,sign up,cart where customers can add product to their cart and would be notified with an email on placing the order and an inventory clerk module through which the retailers can upload and manage their products.",
     "link": "https://amazon-website-assignment.herokuapp.com/",
     "imgSrc": "/Amazon.png",
-    "Description": "",
+    "Description": "I have created a replica of the Amazon website.This includes different modules like registration,sign up,cart where customers can add product to their cart and would be notified with an email on placing the order and an inventory clerk module through which the retailers can upload and manage their products.",
     "github":"https://github.com/manalsolanki/Amazon-Assignment",
-    "roles":["Developer","Designer"]
+    "roles":"Developer, Designer",
+    "Technologies":"NodeJs , Express handlerbars and Mongodb"
+},
+{
+    "name": "Beats On Travel",
+    "shortDescription": "I have created a replica of the Amazon website using nodeJs , express handlerbars and Mongodb.This includes different modules like registration,sign up,cart where customers can add product to their cart and would be notified with an email on placing the order and an inventory clerk module through which the retailers can upload and manage their products.",
+    "link": "https://beats-on-travel.netlify.app/",
+    "imgSrc": "/beatsOnTravel.png",
+    "Description": "I have created a replica of the Amazon website.This includes different modules like registration,sign up,cart where customers can add product to their cart and would be notified with an email on placing the order and an inventory clerk module through which the retailers can upload and manage their products.",
+    "github":"https://github.com/manalsolanki/API-Assignment",
+    "roles":"Developer, Designer",
+    "Technologies":"Youtube Api, Google Distance Matrix Api"
 },
 {
     "name": "Addidas Landing Page",
@@ -14,7 +46,8 @@ const projects = [{
     "imgSrc": "/final-design.png",
     "Description": "",
     "github":"https://github.com/manalsolanki/landingpage-adidas",
-    "roles":["Designer","Developer"]
+    "roles":"Designer, Developer",
+    "Technologies":"Adobe XD, Photoshop"
 },
 {
     "name": "Event Microsites",
@@ -23,8 +56,9 @@ const projects = [{
     "imgSrc": "/event.png",
     "Description": "",
     "github":"https://github.com/manalsolanki/EventMicrosites",
-    "roles":["Designer","Developer"],
-    "wireframes":""
+    "roles":"Designer, Developer",
+    "wireframes":"",
+    "Technologies":"Adobe XD, Photoshop, HTML, CSS"
 },
 {
     "name": "Travelocity Replica",
@@ -33,7 +67,8 @@ const projects = [{
     "imgSrc": "/travelocity.png",
     "Description": "",
     "github":"",
-    "roles":["Designer","Developer"]
+    "roles":"Designer, Developer",
+    "Technologies":"Adobe XD, Illustrator, HTML, CSS"
 },
 ]
 
